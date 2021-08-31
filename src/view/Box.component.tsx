7af506f685356d3cce650e16325710b68412cedd
TiagoFuelber <tfuelber@gmail.com>
2021-08-30 22:29:47 -0300

    [feat]: Add Container and Box components

--- src/view/Box.component.tsx
+++ src/view/Box.component.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BREAKPOINTS } from '../constants';
 
 type ElemProps = { children: React.ReactNode };
 
 export function Box({ children }: ElemProps): JSX.Element {
-  return <div>{children}</div>;
+  return <StyledBox>{children}</StyledBox>;
 }
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const StyledBox = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0px 16px 32px rgba(30, 42, 50, 0.08);
+  padding: 2.2em 1.5em 2.5em;
+
+  @media screen and (min-width: ${BREAKPOINTS.desktop}px) {
+    padding: 2.2em 2.5em 2.5em;
+  }
+`;
--- src/view/Container.component.tsx
+++ src/view/Container.component.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BREAKPOINTS } from '../constants';
 
 type ElemProps = { children: React.ReactNode };
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const Container = ({ children }: ElemProps): JSX.Element => (
-  <div>{children}</div>
+  <StyledContainer>{children}</StyledContainer>
 );
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const StyledContainer = styled.div`
+  width: 100%;
+
+  @media screen and (min-width: ${BREAKPOINTS.desktop}px) {
+    max-width: 560px;
+    margin: 0 auto;
+  }
+`;
